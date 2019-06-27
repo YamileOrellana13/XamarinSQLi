6f4643574b55d53a82097ddfdb2117bbf1754164
yamileandreaorellanasejas@gmail.com <yamileandreaorellanasejas@gmail.com>
2019-06-27 11:16:34 -0400

    Commit de Implementacion

---
+++ XamarinVentas/XamarinVentas/XamarinVentas/Views/ListaVentas.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinVentas.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ListaVentas : ContentPage
+	{
+		public ListaVentas ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
